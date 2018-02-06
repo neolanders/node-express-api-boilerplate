dd9d84f90167a567006489b1d6f8bf96929af1ac
Eric Alter <neolanders@hotmail.com>
2018-02-06 08:15:39 -0800

    Working progress

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,19 @@
 // CUSTOM JS FILE //
+$(function(){
+    $("#modal-launcher, #modal-background, #modal-close").click(function() {
+        $("#modal-content, #modal-background").toggleClass("active");
+    });
+
+    $('a[href$="display"]').click(function() {
+        $("#modal-content, #modal-background").toggleClass("active");
+        $.getJSON('/api/get', function(data) {
+            $('#modal-content .inner-content .add-form').hide();
+            console.log(data);
+            var animals = data.animals;
+            animals.forEach(function(e){
+                var htmlToAppend = '<p>'+e.name+'</p>';
+                $('#modal-content .inner-content').empty().append(htmlToAppend);
+            })
+        }); 
+    });
+});
